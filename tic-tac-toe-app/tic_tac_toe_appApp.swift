178c3d53209f3145bbdbbcc8f56b29f6dcac7bed
溝井迅 <mizoi.jin@ditgroup.jp>
2024-10-14 01:08:26 +0900

    Initial Commit

---
+++ tic-tac-toe-app/tic_tac_toe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toe_appApp.swift
+//  tic-tac-toe-app
+//
+//  Created by mizoi.jin on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
